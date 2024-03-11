62b2acb1e0369acee4f27dc54cd9fede9fb9bcee
ilayc11 <ilayc11@gmail.com>
2024-03-11 17:48:47 +0200

    Deleting unecessery, creating administration

--- Administration/run_frontend_in_gcp.sh
+++ Administration/run_frontend_in_gcp.sh
@@ -20,7 +20,7 @@ gcloud compute addresses list
 
 # note the IP address printed above, that's your extrenal IP address.
 # Enter it here: 
-INSTANCE_IP="34.72.0.179"
+INSTANCE_IP="104.197.62.166"
 
 # 2. Create Firewall rule to allow traffic to port 8080 on the instance
 gcloud compute firewall-rules create default-allow-http-8080 \
