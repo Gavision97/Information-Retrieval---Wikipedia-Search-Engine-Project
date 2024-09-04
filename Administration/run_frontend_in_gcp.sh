71ceb6bf7619e3719e20b23add671dab3c12f9e8
ilayc11 <ilayc11@gmail.com>
2024-09-04 18:53:23 +0300

    Removing ID

--- Administration/run_frontend_in_gcp.sh
+++ Administration/run_frontend_in_gcp.sh
@@ -3,7 +3,7 @@ REGION=us-central1
 ZONE=us-central1-c
 PROJECT_NAME="irprojectilayvictor"
 IP_NAME="$PROJECT_NAME-ip"
-GOOGLE_ACCOUNT_NAME="vicgav97" # without the @post.bgu.ac.il or @gmail.com part
+GOOGLE_ACCOUNT_NAME=XXXXX # without the @post.bgu.ac.il or @gmail.com part
 
 # 0. Install Cloud SDK on your local machine or using Could Shell
 # check that you have a proper active account listed
--- search_frontend.py
+++ search_frontend.py
@@ -72,22 +72,7 @@ async def body_search(query_stemmed, N):
 
 @app.route("/search")
 async def search():
-    ''' Returns up to a 100 of your best search results for the query. This is
-        the place to put forward your best search engine, and you are free to
-        implement the retrieval whoever you'd like within the bound of the
-        project requirements (efficiency, quality, etc.). That means it is up to
-        you to decide on whether to use stemming, remove stopwords, use
-        PageRank, query expansion, etc.
-
-        To issue a query navigate to a URL like:
-         http://YOUR_SERVER_DOMAIN/search?query=hello+world
-        where YOUR_SERVER_DOMAIN is something like XXXX-XX-XX-XX-XX.ngrok.io
-        if you're using ngrok on Colab or your external IP on GCP.
-    Returns:
-    --------
-        list of up to 100 search results, ordered from best to worst where each
-        element is a tuple (wiki_id, title).
-    '''
+    
     res = []
     query = request.args.get('query', '')
     if len(query) == 0:
